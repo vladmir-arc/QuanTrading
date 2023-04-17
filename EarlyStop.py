38708f178a1f7154d17f760998bc2a3909dda19f
YEtintin <1377920521@qq.com>
2023-04-17 21:05:11 +0800

    Fix some Loss bug

--- EarlyStop.py
+++ EarlyStop.py
@@ -3,19 +3,13 @@ import numpy as np
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0,  trace_func=print):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
             verbose (bool): If True, prints a message for each validation loss improvement.
-                            Default: False
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
-            path (str): Path for the checkpoint to be saved to.
-                            Default: 'checkpoint.pt'
             trace_func (function): trace print function.
-                            Default: print
         """
         self.patience = patience
         self.verbose = verbose
@@ -24,20 +18,17 @@ class EarlyStopping:
         self.early_stop = False
         self.val_loss_min = np.Inf
         self.delta = delta
-        self.path = path
         self.trace_func = trace_func
 
     def __call__(self, loss):
-
-        score = -loss
-
         if self.best_score is None:
-            self.best_score = score
-        elif score < self.best_score + self.delta:
+            self.best_score = loss
+        elif (loss - (self.best_score * (1 + self.delta))) > 0:
             self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            # self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
-            self.best_score = score
+            self.best_score = loss
+            print(self.best_score)
             self.counter = 0
